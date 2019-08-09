d1156d59fbc89d1b34638e04ea58f53a8d7b3a58
Ryan Dancy <ryan@keal.ca>
2019-08-09 16:20:29 -0400

    Add ChunkArray
    ChunkArray is a wrapper over std::unordered_map<std::pair<int, int>,
    Chunk>. Its purpose is as a container for Chunks. It is a separate class
    rather than a typedef for std::unordered_map<std::pair<int, int>, Chunk>
    in order to enable manipulation of Chunks via Topology once that is
    implemented.

--- gol/Chunk.cpp
+++ gol/Chunk.cpp
@@ -1,9 +1,49 @@
 #include "Chunk.h"
 
+// Chunk
+
 void Chunk::tick() {
   // TODO
 }
 
 void Chunk::tick(Side side) {
   // TODO
+}
+
+// ChunkArray
+
+ChunkArray::size_type ChunkArray::size() {
+  return map.size();
+}
+
+bool ChunkArray::contains(int x, int y) const {
+  return map.count({x, y}) == 1;
+}
+
+Chunk& ChunkArray::get(int x, int y) {
+  return map[{x, y}];
+}
+
+Chunk& ChunkArray::operator[](std::pair<int, int> xy) {
+  return map[xy];
+}
+
+bool ChunkArray::erase(int x, int y) {
+  return map.erase({x, y}) > 0;
+}
+
+ChunkArray::iterator ChunkArray::begin() noexcept {
+  return map.begin();
+}
+
+ChunkArray::iterator ChunkArray::end() noexcept {
+  return map.end();
+}
+
+ChunkArray::const_iterator ChunkArray::begin() const noexcept {
+  return map.begin();
+}
+
+ChunkArray::const_iterator ChunkArray::end() const noexcept {
+  return map.end();
 }
--- gol/Chunk.h
+++ gol/Chunk.h
@@ -1,10 +1,14 @@
 #ifndef GAME_OF_LIFE_CHUNK_H
 #define GAME_OF_LIFE_CHUNK_H
 
+#include <unordered_map>
+#include <utility>
+
 #include "Side.h"
 
 #define CHUNK_SIZE 20
 
+// A "chunk" of cells which are all processed at once
 class Chunk {
 public:
   void tick(); // move the entire Chunk to the next generation
@@ -14,5 +18,36 @@ private:
   bool cells[CHUNK_SIZE][CHUNK_SIZE];
 };
 
+// A 2D-indexed list of Chunks - a thin wrapper over std::unordered_map
+// This isn't just a typedef for std::unordered_map<std::pair<int, int>, Chunk> because it'll encapsulate a Topology
+// TODO have a Topology, once that's implemented
+class ChunkArray {
+  typedef std::unordered_map<std::pair<int, int>, Chunk>::iterator iterator;
+  typedef std::unordered_map<std::pair<int, int>, Chunk>::const_iterator const_iterator;
+  typedef std::unordered_map<std::pair<int, int>, Chunk>::size_type size_type;
+  
+public:
+  // How many Chunks are stored?
+  size_type size();
+  
+  // Get the Chunk at (x, y), creating one if not present
+  Chunk& get(int x, int y);
+  Chunk& operator[](std::pair<int, int> xy);
+  
+  // Does this ChunkArray contain a Chunk at (x, y)?
+  bool contains(int x, int y) const;
+  
+  // Erase the Chunk at (x, y) if present, returning whether a Chunk was erased
+  bool erase(int x, int y);
+  
+  // Iterators
+  iterator begin() noexcept;
+  iterator end() noexcept;
+  const_iterator begin() const noexcept;
+  const_iterator end() const noexcept;
+  
+private:
+  std::unordered_map<std::pair<int, int>, Chunk> map;
+};
 
 #endif //GAME_OF_LIFE_CHUNK_H
