78fb40776c488e167b728c1538d2fbfce4634558
Ryan Dancy <ryan@keal.ca>
2019-08-12 16:02:36 -0400

    Add more Topology subclasses
    Add UnboundedTopology, which has no boundaries and continues forever.
    Add WrappingTopology, which has boundaries like FixedTopology, but
    wraps around at the edges, forming a torus.
    Add BoundedTopology, an intermediary abstract Topology subclass which
    represents a Topology with a boundary - a width and a height.
    UnboundedTopology and WrappingTopology are BoundedTopologies.

--- gol/Topology.cpp
+++ gol/Topology.cpp
@@ -1,19 +1,47 @@
 #include <stdexcept>
 #include "Topology.h"
 
-// FixedTopology
+// BoundedTopology
 
-FixedTopology::FixedTopology(unsigned int width, unsigned int height) : width(width), height(height) {
-  if (width == 0 || height == 0) {
-    throw std::out_of_range("Cannot create a FixedTopology with zero width or height");
+BoundedTopology::BoundedTopology(int width, int height) : width(width), height(height) {
+  if (width <= 0 || height <= 0) {
+    throw std::out_of_range("Cannot create a BoundedTopology with negative or zero width or height");
   }
 }
 
-bool FixedTopology::valid(int x, int y) const {
+bool BoundedTopology::valid(int x, int y) const {
   return x >= 0 && y >= 0 && x <= width && y <= height;
 }
+
+// FixedTopology
 
 bool FixedTopology::transform(int& x, int& y) {
   // FixedTopology isn't going to actually transform it, it'll just declare it invalid if it's outside the limits
   return valid(x, y);
 }
+
+// WrappingTopology
+
+bool WrappingTopology::transform(int& x, int& y) {
+  // wrapping is accomplished with modulus
+  x %= width;
+  if (x < 0) x += width;
+  y %= height;
+  if (y < 0) y += height;
+  return true;
+}
+
+// UnboundedTopology
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter" // x and y aren't used in UnboundedTopology
+
+bool UnboundedTopology::valid(int x, int y) const {
+  return true; // all chunks are valid
+}
+
+bool UnboundedTopology::transform(int& x, int& y) {
+  return true; // no transformations are performed since all chunks are valid
+}
+
+#pragma clang diagnostic pop
--- gol/Topology.h
+++ gol/Topology.h
@@ -15,23 +15,47 @@ public:
   virtual bool transform(int& x, int& y) = 0;
 };
 
-// This is a Topology with a fixed size - it simply stops after a certain number of Chunks.
-// All other chunks should be treated as completely empty/dead.
-class FixedTopology : public Topology {
+// This is an abstract Topology with bounds, meaning it has a width and a height. Chunks are valid iff they are within
+// (0, 0) and (width, height), inclusive.
+class BoundedTopology : public Topology {
 public:
-  // Initialize with a width and height, in units of chunks.
-  // Will throw std::out_of_range if the width or height is 0.
-  FixedTopology(unsigned int width, unsigned int height);
+  // Initialize the bounds with a width and height, in units of chunks.
+  // Will throw std::out_of_range if the width or height are negative or zero.
+  BoundedTopology(int width, int height);
   
-  // This implementation simply returns whether if 0 <= x <= width and 0 <= y <= height.
+  // Simply return whether 0 <= x <= width and 0 <= y <= height.
   bool valid(int x, int y) const override;
   
+  // Note: width and height are signed to prevent headaches with arithmetic with signed coordinates
+  const int width; // The width of the boundary of the topology, in chunks
+  const int height; // The height of the boundary of the topology, in chunks
+};
+
+// This is a Topology with a fixed size - it simply stops after a certain number of Chunks.
+// All other chunks should be treated as completely empty/dead.
+class FixedTopology : public BoundedTopology {
+  using BoundedTopology::BoundedTopology;
+  
+public:
   // If (x, y) is valid, this method will do nothing and return true. If not, it will return false, signifying
   // that the chunk should be treated as fully empty.
   bool transform(int& x, int& y) override;
+};
+
+// This is a Topology which wraps around in both the x and y directions. Topologically, it forms a torus.
+class WrappingTopology : public BoundedTopology {
+  using BoundedTopology::BoundedTopology;
   
-  const unsigned int width; // The width of the fixed boundary of the topology, in chunks
-  const unsigned int height; // The height of the fixed boundary of the topology, in chunks
+public:
+  // This implementation will wrap x and y as many times as necessary. It will always return true.
+  bool transform(int& x, int& y) override;
+};
+
+// This is a Topology with no restrictions whatsoever. All chunks are valid and no transformations are performed.
+class UnboundedTopology : public Topology {
+public:
+  bool valid(int x, int y) const override; // Always returns true - all chunks are valid.
+  bool transform(int& x, int& y) override; // Always returns true and does not modify x and y.
 };
 
 #endif //GAME_OF_LIFE_TOPOLOGY_H
